de395bb15c16e43621f8b2554e9ee935627db78a
Martin Ruefenacht <martin.ruefenacht@lrz.de>
2022-10-10 11:03:57 +0200

    Add user helpers

--- bqp_database_access/tokens.py
+++ bqp_database_access/tokens.py
@@ -6,7 +6,7 @@ import pony.orm as pony
 from bcrypt import hashpw
 
 from ._constants import PEPPER
-from ._database import _open_database
+from ._database import open_database
 
 
 class TokenError(Exception):
@@ -14,44 +14,44 @@ class TokenError(Exception):
 
 
 class TooManyTokensError(TokenError):
-    """This exception occurs when a user attempts to create too many tokens."""
+    """This exception occurs when a identity attempts to create too many tokens."""
 
-    def __init__(self, count: int, user: str):
+    def __init__(self, count: int, identity: str):
         self.count = count
-        self.user = user
+        self.identity = identity
 
         super().__init__(f"Users are not allowed to have more than {count} tokens.")
 
 
 class TokenExistsError(TokenError):
-    """This exception occurs when a user attempts to create a token with the same name."""
+    """This exception occurs when a identity attempts to create a token with the same name."""
 
     def __init__(self, token_name: str):
         super().__init__(f"Token '{token_name}' already exists.")
 
 
 class TokenExpirationBeforeNow(TokenError):
-    """This exception occurs when a token is created with an expiration that is before the current time."""
+    """This exception occurs when a token is created with an expiration that is before now."""
 
     def __init__(self, name: str):
         super().__init__(f"Token {name} being created with expiration before now.")
 
 
 class TokenNotFound(TokenError):
-    """This exception occurs when a user attempts to find a token which doesn't exist."""
+    """This exception occurs when a identity attempts to find a token which doesn't exist."""
 
     def __init__(self, name: str, owner: str):
-        super().__init__(f"Token {name} not found of user {owner}.")
+        super().__init__(f"Token {name} not found of identity {owner}.")
 
 
 def add_new_token(name: str, owner: str, token: str, expiration: datetime) -> None:
-    """Add a new token for a given user to the database with an expiration."""
+    """Add a new token for a given identity to the database with an expiration."""
 
     # check if expiration is too soon
     if datetime.now() >= expiration:
         raise TokenExpirationBeforeNow(name)
 
-    quantum_db = _open_database()
+    quantum_db = open_database()
 
     # TODO add as configuration
     token_count_limit = 100
@@ -78,10 +78,10 @@ def add_new_token(name: str, owner: str, token: str, expiration: datetime) -> No
     quantum_db.commit()
 
 
-def revoke_token_by_name_and_user(name: str, owner: str) -> None:
-    """Revoke a given token by name and user."""
+def revoke_token_by_name_and_identity(name: str, owner: str) -> None:
+    """Revoke a given token by name and identity."""
 
-    quantum_db = _open_database()
+    quantum_db = open_database()
 
     token = quantum_db.Token.get(owner=owner, remember_name=name, revoked=False)
 
@@ -94,10 +94,10 @@ def revoke_token_by_name_and_user(name: str, owner: str) -> None:
     quantum_db.commit()
 
 
-def fetch_active_tokens_of_user(owner: str) -> tuple[str, ...]:
+def fetch_active_tokens_of_identity(owner: str) -> tuple[str, ...]:
     """Fetch all tokens"""
 
-    quantum_db = _open_database()
+    quantum_db = open_database()
 
     return tuple(
         token.remember_name
