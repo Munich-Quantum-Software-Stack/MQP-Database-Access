7a015a1f84f34455437c9136a46902a9473a23cb
Martin Ruefenacht <martin.ruefenacht@lrz.de>
2022-10-17 09:57:20 +0200

    Added budget support

---
+++ bqp_database_access/budgets.py
@@ -0,0 +1,11 @@
+"""This module contains all helpers related to budgets in the database."""
+
+from ._database import open_database
+
+
+def fetch_budget_names_of_identity(identity: str) -> tuple[str]:
+    """Fetch all budgets."""
+
+    quantum_db = open_database()
+
+    return tuple(budget.name for budget in quantum_db.User.budgets)
