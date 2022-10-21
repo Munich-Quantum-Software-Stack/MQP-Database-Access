6b9288e1b059860682c06dbcab872355a085b799
Martin Ruefenacht <martin.ruefenacht@lrz.de>
2022-10-21 14:44:16 +0200

    Added pointers concept

--- bqp_database_access/status.py
+++ bqp_database_access/status.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+from random import choice
 
 from ._database import open_database
 
 
-def fetch_current_announcements() -> tuple[str]:
+def fetch_current_announcements() -> tuple["Announcement"]:
     """Fetch all mandatory announcements."""
 
     quantum_db = open_database()
@@ -13,3 +14,16 @@ def fetch_current_announcements() -> tuple[str]:
     )
 
     return tuple(announcements)
+
+
+def get_random_pointers(count: int) -> list["Pointer"]:
+
+    quantum_db = open_database()
+
+    pointers = quantum_db.Pointer.select(
+        lambda p: p.start_time < datetime.now() and p.end_time > datetime.now()
+    )
+
+    weights = [pointer.weight for pointer in pointers]
+
+    return choice(pointers, weights, k=count)
