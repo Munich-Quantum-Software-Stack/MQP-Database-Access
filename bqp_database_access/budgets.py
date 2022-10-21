14095738ed2f70dda723079b96027ae6fb3465fd
Martin Ruefenacht <martin.ruefenacht@lrz.de>
2022-10-21 12:29:00 +0200

    Fixed fetching resources

--- bqp_database_access/budgets.py
+++ bqp_database_access/budgets.py
@@ -10,12 +10,10 @@ def fetch_budget_names_of_identity(identity: str) -> tuple[str]:
 
     user = quantum_db.User.get(email=identity)
 
-    direct_budgets = {budget.name for budget in user.budgets.select()[:]}
+    direct_budgets = {budget.name for budget in user.budgets}
 
     user_group_budgets = {
-        budget.name
-        for user_group in user.user_groups.select()[:]
-        for budget in user_group.budgets.select()[:]
+        budget.name for user_group in user.user_groups for budget in user_group.budgets
     }
 
     return tuple(direct_budgets.union(user_group_budgets))
--- bqp_database_access/resources.py
+++ bqp_database_access/resources.py
@@ -8,10 +8,17 @@ def fetch_resources_available_to_identity(identity: str) -> tuple["Resource"]:
 
     quantum_db = open_database()
 
-    return tuple(
-        set(
-            resource
-            for budget in quantum_db.User.get(email=identity).budgets
-            for resource in budget.resources
-        )
-    )
+    user = quantum_db.User.get(email=identity)
+
+    direct_resources = {
+        resource for budget in user.budgets for resource in budget.resources
+    }
+
+    user_group_budget_resources = {
+        resource
+        for user_group in user.user_groups
+        for budget in user_group.budgets
+        for resource in budget.resources
+    }
+
+    return tuple(direct_resources.union(user_group_budget_resources))
