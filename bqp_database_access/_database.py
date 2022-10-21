37694d08e18aa2162caefd219528e6220e14c3eb
Martin Ruefenacht <martin.ruefenacht@lrz.de>
2022-10-21 13:00:29 +0200

    Added submitted datetime to job entity

--- bqp_database_access/_database.py
+++ bqp_database_access/_database.py
@@ -53,6 +53,7 @@ def _define_entities(database: Database):
         cancel_reason = Optional(str)
         id = PrimaryKey(int, auto=True)
         shots_complete = Required(int)
+        submitted = Required(datetime)
 
     class Budget(database.Entity):
         _table_ = "budget"
