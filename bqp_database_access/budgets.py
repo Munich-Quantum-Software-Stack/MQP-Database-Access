5dfae187a66e5c09682ccfa80f9531974c3a2a53
Martin Ruefenacht <martin.ruefenacht@lrz.de>
2022-10-21 12:36:01 +0200

    Changed to return budgets entirely

--- bqp_database_access/budgets.py
+++ bqp_database_access/budgets.py
@@ -3,17 +3,17 @@
 from ._database import open_database
 
 
-def fetch_budget_names_of_identity(identity: str) -> tuple[str]:
+def fetch_budgets_of_identity(identity: str) -> tuple["Budget"]:
     """Fetch all budgets."""
 
     quantum_db = open_database()
 
     user = quantum_db.User.get(email=identity)
 
-    direct_budgets = {budget.name for budget in user.budgets}
+    direct_budgets = {budget for budget in user.budgets}
 
     user_group_budgets = {
-        budget.name for user_group in user.user_groups for budget in user_group.budgets
+        budget for user_group in user.user_groups for budget in user_group.budgets
     }
 
     return tuple(direct_budgets.union(user_group_budgets))
