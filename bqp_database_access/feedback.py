e3a9fb52a0ce10ba62798a4f9e856ffbcd00275e
Kumar, Nitish <nitish.kumar@lrz.de>
2024-07-05 14:34:30 +0200

    Feedback

---
+++ bqp_database_access/feedback.py
@@ -0,0 +1,21 @@
+"""This module contains all helpers for the feedback handling."""
+
+from datetime import datetime
+from ._database import open_database
+
+
+def create_feedback_for_identity(
+	 owner : str, rating : int, category : str, note: str 
+) -> None:
+
+    quantum_db = open_database()
+
+    quantum_db.Feedback(
+	owner=owner,
+	rating=rating,
+	category=category,
+	feedbackDate=datetime.now(),
+	note=note
+    )
+
+    quantum_db.commit()
