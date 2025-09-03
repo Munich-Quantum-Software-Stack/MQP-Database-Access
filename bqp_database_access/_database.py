6964d908e74ea6a626899ec5ed1bae8608285384
Utkarsh Saraswat <utkarsh.saraswat@lrz.de>
2025-09-03 15:57:27 +0000

    fixing bracket issue

--- bqp_database_access/_database.py
+++ bqp_database_access/_database.py
@@ -297,8 +297,8 @@ def _define_database(create_tables: bool = False, **db_params):
 
 
 def open_database(create_tables: bool = False):
-    if os.getenv("QUANTUM_DB_TESTING") is not None and [os.getenv("USER_TESTING") == ""
-    or os.getenv("USER_TESTING") is None]:
+    if os.getenv("QUANTUM_DB_TESTING") is not None and (os.getenv("USER_TESTING") == ""
+    or os.getenv("USER_TESTING") is None):
         return _define_database(
             create_tables=create_tables,
             provider="sqlite",
