b4b244cd9dc44995292a431a812a73d13662c1dc
Tobias Bauer <di35hej@badwlrz-cl14639>
2025-09-03 11:33:12 +0200

    fixed syntax error

--- bqp_database_access/jobs.py
+++ bqp_database_access/jobs.py
@@ -52,7 +52,7 @@ def fetch_by_identity_pages(identity: str, page: int, jobs_per_page: int,
 
     start_list = page*jobs_per_page
     
-    if order_by == "ID"
+    if order_by == "ID":
         query = query.order_by(quantum_db.CircuitJob.id)
     elif order_by == "DATE":
         query = query.order_by(quantum_db.CircuitJob.timestamp_submitted)
