91128115ecf4c62909131f1bce8c6bc31bbaec3b
Utkarsh Saraswat <utkarsh.saraswat@lrz.de>
2025-09-08 11:25:05 +0000

    fixing pylint issue

--- bqp_database_access/jobs.py
+++ bqp_database_access/jobs.py
@@ -307,7 +307,8 @@ def fetch_all_waiting_hamiltonian_jobs() -> list["HamiltonianJob"]: # type: igno
     return list(quantum_db.HamiltonianJob.select(status="WAITING"))
 
 @db_session
-def retrieve_timestamps(num_recent_timestamps: int, timestamp_scheduled: bool = False , timestamp_cancelled: bool = False) -> dict: # type: ignore
+def retrieve_timestamps(num_recent_timestamps: int, timestamp_scheduled: bool = False,
+                        timestamp_cancelled: bool = False) -> dict: # type: ignore
     data = {
         "timestamp_submitted" : [],
         "timestamp_scheduled" : [],
