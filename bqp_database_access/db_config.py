4be7f69cd6f91e95d7e3369486e02e9fff86425a
root <Nazim.Oezmeral@lrz.de>
2025-12-22 17:16:37 +0100

    Remove tool-specific comments and clarify intent

--- bqp_database_access/db_config.py
+++ bqp_database_access/db_config.py
@@ -2,7 +2,7 @@
 import os
 
 
-def set_test_env(filename: str = "test_db.db"): # ChatGPT
+def set_test_env(filename: str = "test_db.db"):
     """
     Set all environment variables required for the test environment.
 
@@ -10,14 +10,12 @@ def set_test_env(filename: str = "test_db.db"): # ChatGPT
     the database connection for testing purposes.
     """
 
-    # ChatGPT: Make sure Postgres settings don't get used in tests
     os.environ["QUANTUM_DB_HOST"] = ""
     os.environ["QUANTUM_DB_USER"] = ""
     os.environ["QUANTUM_DB_PASS"] = ""
     # os.environ["QUANTUM_DS_HOST"] = "ldaps:localhost"
     # os.environ["USER_TESTING"] = "true"
 
-    # ChatGPT: extra two lines
     os.environ["QUANTUM_DB_TESTING"] = "TRUE"
     os.environ["QUANTUM_DB_FILENAME"] = filename
 
@@ -25,8 +23,8 @@ def set_test_env(filename: str = "test_db.db"): # ChatGPT
 def reset_test_env():
     # os.environ["QUANTUM_DB_TESTING"] = "TRUE"
 
-    os.environ.pop("QUANTUM_DB_TESTING", None) # ChatGPT
-    os.environ.pop("QUANTUM_DB_FILENAME", None) # ChatGPT
+    os.environ.pop("QUANTUM_DB_TESTING", None)
+    os.environ.pop("QUANTUM_DB_FILENAME", None)
 
     os.environ["QUANTUM_DB_USER"] = ""
     os.environ["QUANTUM_DB_PASS"] = ""
