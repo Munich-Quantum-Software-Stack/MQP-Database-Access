9a22ef7c586d991fff5a801ad5f8de21bc467a8b
Nazim Ege Özmeral <Nazim.Oezmeral@lrz.de>
2025-12-24 12:25:30 +0000

    removed unnecessary imports

--- bqp_database_access/__init__.py
+++ bqp_database_access/__init__.py
@@ -1,17 +1,12 @@
+
 from eliot import add_destinations
 import os
-from . import db_config as config # from . import config
+from . import db_config as config
+
+from . import jobs
 
 if os.getenv("QUANTUM_DB_TESTING") is None:
     from eliot.journald import JournaldDestination
-
-
-from . import login
-from . import tokens
-from . import jobs
-from . import resources
-from . import feedbacks
-from . import request_access
 
 if os.getenv("QUANTUM_DB_TESTING") is None:
     add_destinations(JournaldDestination())
