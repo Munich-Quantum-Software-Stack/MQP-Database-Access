148989df0314daa52fcf4930854649227ccd6f2f
Ege Özmeral <ege.oezmeral@gmail.com>
2026-01-08 18:11:06 +0000

    Corrected __init__.py, jobs.py, conftest.py and modified the tests test_fetch_by_id.py and test_users.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-from bqp_database_access import create_app # from bqp_dashboard_backend import create_app
+from bqp_database_access import create_app
 from bqp_database_access._database import open_database
 import bqp_database_access as database_access
 from pony.orm import db_session
