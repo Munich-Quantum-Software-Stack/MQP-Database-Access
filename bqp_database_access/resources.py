e37162d89af4076720890c39914d56febc272fd6
Trang Huynh <Trang.Huynh@lrz.de>
2026-04-15 18:15:12 +0200

    Correct feature's implement: block users who are not in EQE group to resource EQE1

--- bqp_database_access/resources.py
+++ bqp_database_access/resources.py
@@ -102,7 +102,8 @@ def fetch_resource_names_restricted_to_identity(identity: str) -> list[str]:
         restricted_resource_names.append("Q5")
         restricted_resource_names.append("Q20")
         restricted_resource_names.append("QExa20")
-    elif "EQE" in _user_group_names:
+    
+    if "EQE" not in _user_group_names:
         # block access to EQE1 device
         restricted_resource_names.append("EQE1")
 
--- tests/test_resources.py
+++ tests/test_resources.py
@@ -12,10 +12,10 @@ def test_fetch_resource_names_restricted_to_identity_in_EQE_group(seeded_db):
     identity = "test_eqe_user"
     with db_session:
         restricted_resources = fetch_resource_names_restricted_to_identity(identity=identity)
-        assert("EQE1" in restricted_resources)
+        assert("EQE1" not in restricted_resources)
 
 def test_fetch_resource_names_restricted_to_identity_not_in_EQE_group(seeded_db):
     identity = "test_user"
     with db_session:
         restricted_resources = fetch_resource_names_restricted_to_identity(identity=identity)
-        assert("EQE1" not in restricted_resources)
+        assert("EQE1" in restricted_resources)
